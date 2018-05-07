bee54800f70062ae39281c890a29e29dbf58f14d
Guillaume-Martin Ratté <guillaumemratte@gmail.com>
2018-05-07 07:43:42 -0400

    more dev changes

---
+++ src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service'
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return Observable.of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return Observable.of(HEROES.find(hero => hero.id === id));
+  }
+
+}
